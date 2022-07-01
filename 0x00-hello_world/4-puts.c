69878530e7de98edaaff7dd52a6f9ffc9e5aa318
jogiz12 <yosephmelele@gmail.com>
2022-07-01 00:21:44 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-gcc 4-puts.c
 #include <stdio.h>
-
-/
- * main - entry point,
- *  *
- *   * Return:  0
- *    */
-
- int main(void)
+/**
+* main - entry point
+*
+*   * Return: 0 after printing the function
+*/
+int main(void)
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
