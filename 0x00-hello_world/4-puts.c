cd06231a215d6d2152866f9fbebceaa963b303bd
jogiz12 <yosephmelele@gmail.com>
2022-06-30 21:34:39 -0700

    My first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+gcc 4-puts.c
+#include <stdio.h>
+
+/
+ * main - entry point,
+ *  *
+ *   * Return:  0
+ *    */
+
+ int main(void)
+{
+	  puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+}
