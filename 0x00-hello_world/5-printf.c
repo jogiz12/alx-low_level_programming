fdbe1bff7d0e1b8cd4b7bfa1e383635a80de2721
jogiz12 <yosephmelele@gmail.com>
2022-07-01 02:37:22 -0700

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - print a string       
-*Return: puts string
-*/
+ *main - print a string
+ *Return: puts string
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
