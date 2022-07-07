8feb861345a0fae7628a5bb891a97ba65a584859
jogiz12 <yosephmelele@gmail.com>
2022-07-07 13:41:24 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - Do something
+ *Return: Return something
+ *@a: do something
+ *@b: do something
+ */
+int mul(int a, int b)
+{
+int multi;
+multi = a * b;
+return (multi);
+}
