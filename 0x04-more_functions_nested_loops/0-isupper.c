32e6ef2ee3879c9ed2888305f5399fa541c3360e
jogiz12 <yosephmelele@gmail.com>
2022-07-07 13:13:15 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - Do something
+ * Return: Return something
+ * @c: do something
+ */
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
