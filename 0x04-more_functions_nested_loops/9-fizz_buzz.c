b220d744e8eef8d391c3d4a2651762cfcd85c066
jogiz12 <yosephmelele@gmail.com>
2022-07-07 14:14:40 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <main.h>
+/**
+ *main - do something
+ *Return: something
+ */
+int main(void)
+{
+int contador;
+for (contador = 1; contador < 101; contador++)
+{
+if ((contador % 3 == 0) && (contador % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (contador % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (contador % 5 == 0)
+{
+if (contador < 100)
+printf("Buzz ");
+else
+printf("Buzz");
+}
+if (!((contador % 3 == 0) || (contador % 5 == 0)))
+{
+printf("%d ", contador);
+}
+}
+printf("\n");
+return (0);
+}
