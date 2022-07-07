8f96ff35b219ac5d702e0708f1d520da1427bae7
jogiz12 <yosephmelele@gmail.com>
2022-07-07 13:46:05 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers - Do something
+ *Return: Return something
+ */
+void print_numbers(void)
+{
+int cont;
+for (cont = 0; cont < 10; cont++)
+{
+_putchar(cont + 48);
+}
+_putchar('\n');
+}
