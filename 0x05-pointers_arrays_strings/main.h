b15397d41c483b0bb1551dbd454e7f739b98e4f8
jogiz12 <yosephmelele@gmail.com>
2022-07-11 14:29:34 -0700

    My commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 /**
  *reset_to_98 - takes pointer int and update the value to 98
  */
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,3 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void generationRandomPassword(int N);
