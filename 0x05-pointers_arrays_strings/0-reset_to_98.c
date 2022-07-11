e103726dcfd2dfca8fa109b3496586bd47cdfe4e
jogiz12 <yosephmelele@gmail.com>
2022-07-11 13:25:45 -0700

    My first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
-#include "main.c"
+#include "main.h"
 /**
- *reset_to_98 - do something
- *@n: do something
+ *reset_to_98 - set value of int to 98
+ *@n: address of value to change
+ *Description: resets *n to 98
+ *Return: nothing
  */
 void reset_to_98(int *n)
 {
