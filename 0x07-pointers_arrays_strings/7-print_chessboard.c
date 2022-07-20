3f018a609c9b71faa4862606c0fd82fca87f4f96
jogiz12 <yosephmelele@gmail.com>
2022-07-20 04:56:31 -0700

    My commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_chessboard - Prints a chessboard.
+ * @a: The chessboard to be printed.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int woma1, woma2;
+	for (woma1 = 0; a[woma1][7]; woma1++)
+	{
+		for (woma2 = 0; woma2 < 8; woma2++)
+			_putchar(a[woma1][woma2]);
+		_putchar('\n');
+	}
+}
