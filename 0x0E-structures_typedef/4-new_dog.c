c44fa416db4a114037b986c44173581a891dc277
jogiz12 <yosephmelele@gmail.com>
2022-08-03 15:01:36 -0700

    My commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,10 +50,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (doggo->name == NULL)
-{
+	{
 	free(doggo);
 	return (NULL);
-}
+	}
 doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 if (doggo->owner == NULL)
 {
