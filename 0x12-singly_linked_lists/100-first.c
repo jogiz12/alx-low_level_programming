90c2e3e2be9541915f36e177eb6f5610de1e968f
jogiz12 <yosephmelele@gmail.com>
2022-08-13 12:47:04 -0700

    My commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+void __attribute__((constructor)) before_main(void);
+/**
+ * before_main - function executed before main
+ * Return: no return
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
